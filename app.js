3fbdbc7e7b005d21657670c271c4082b24112b84
Akshit Kumar <akshitk40@gmail.com>
2021-11-22 07:04:35 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const app = express();
+const proxy = require('express-http-proxy');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+function getHost(req) {
+    return db.get(req.headers.host).value() || undefined;
+}
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', proxy(getHost, {
+    proxyReqPathResolver: async function (req) {
+        let parts = req.url.split('?');
+        let queryString = parts[1];
+        let updatedPath = parts[0].replace(/test/, 'tent');
+        return updatedPath + (queryString ? '?' + queryString : '');
+    },
+    proxyErrorHandler: function (err, res, next) {
+        if (err.toString() === "TypeError: Cannot read property 'request' of undefined") {
+            return next(createError(400, 'The website you are trying to reach does not exist on this Sperse.'));
+        }
+        switch (err && err.code) {
+            case 'ECONNRESET': {
+                return next(createError(503, 'The origin service is currently unavailable.'));
+            }
+            case 'ECONNREFUSED': {
+                return next(createError(502, 'Gateway Error! Can not connect to the origin service.'));
+            }
+            default: {
+                return next(createError(500, 'The server encountered an error.'));
+            }
+        }
+    }
+}));
+
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.code = err.status;
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
